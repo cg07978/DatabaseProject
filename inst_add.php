918602b746957ec32c876e89d01d895a8ad7ceb9
cg07978 <54293611+cg07978@users.noreply.github.com>
2020-11-08 20:03:56 -0500

    Add files via upload

--- inst_add.php
+++ inst_add.php
@@ -34,7 +34,7 @@ $errors = array('name' => '', 'price' => '');
 			if(!preg_match('/^\d+.\d\d$/', $price)) {
 				$errors['price'] = 'Price must be in the form N+.NN';
 			}
-			else if(strlen($price) > 11) {
+			else if(strlen($price) > 9) {
 				$errors['price'] = "That price is way too high.";
 			}
 		}
